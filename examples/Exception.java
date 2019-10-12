4e4914740e0206d8d16266f60f480199a908ece7
Amanda Oliveira Alves <amandaoa6@gmail.com>
2019-10-12 11:45:59 -0300

    exception error

--- examples/Exception.java
+++ examples/Exception.java
@@ -2,6 +2,7 @@ public class Exception {
 
 	public static void main(String[] args) {
         int num1, num2;
+        
         try {
             throw new ArithmeticException("/ by zero");
         } catch (ArithmeticException e) {
