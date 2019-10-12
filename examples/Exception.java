a52753575024b8f9ef033503265548013074bd39
Amanda Oliveira Alves <amandaoa6@gmail.com>
2019-10-12 11:46:44 -0300

    exception passando args

--- examples/Exception.java
+++ examples/Exception.java
@@ -2,9 +2,8 @@ public class Exception {
 
 	public static void main(String[] args) {
         int num1, num2;
-        
         try {
-            throw new ArithmeticException("/ by zero");
+        	throw new ArithmeticException("/ by zero");
         } catch (ArithmeticException e) {
             System.out.print(e);
         }
