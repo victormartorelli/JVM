23f3b3eec04b4c5740bccf0403a45ec2439e42e2
Amanda Oliveira Alves <amandaoa6@gmail.com>
2019-10-12 12:00:16 -0300

    modificando exibição de string

--- examples/HelloWorld.java
+++ examples/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
-	private static String str = "Hello World!";
+	private static String str = "Hello World";
 
 	public static void main(String[] args) {
 		System.out.println(str);
